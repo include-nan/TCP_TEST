aabb4a5bc762485224f46abf5bd85142a576b5e4
include-nan <zjn1210@outlook.com>
2020-12-28 16:43:34 +0800

    完成RDT2.2

---
+++ README.md
@@ -0,0 +1,4 @@
+# TCP_TEST
+ouc.tcp.test
+
+[此分支](https://github.com/include-nan/TCP_TEST/tree/for_report) 用来整理每个版本的信息，依此完成实验报告
--- src/com/ouc/tcp/test/TCP_Receiver.java
+++ src/com/ouc/tcp/test/TCP_Receiver.java
@@ -45,7 +45,8 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
             System.out.println("Receive Computed: " + CheckSum.computeChkSum(recvPack));
             System.out.println("Received Packet" + recvPack.getTcpH().getTh_sum());
             System.out.println("Problem: Packet Number: " + recvPack.getTcpH().getTh_seq() + " + InnerSeq:  " + sequence);
-            tcpH.setTh_ack(-1);
+            // 返回上一个正确的包的ack
+            tcpH.setTh_ack((sequence - 1) * 100 + 1);
             ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
             tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
             //回复ACK报文段
